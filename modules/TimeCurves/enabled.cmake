f4c3bf936ad29624e883ca2c953ade1c1db6a1d6
antoniaschilling <schilling@fim.uni-passau.de>
2023-12-13 16:23:26 +0100

    tapkee include in test + minimal example

--- modules/TimeCurves/enabled.cmake
+++ modules/TimeCurves/enabled.cmake
@@ -9,15 +9,16 @@ if (openiA_TESTING_ENABLED)
 		${CMAKE_CURRENT_SOURCE_DIR}/../libs/base 
 		${CMAKE_CURRENT_BINARY_DIR}/../libs
 		${CMAKE_CURRENT_BINARY_DIR}           # for _export.h
+		${TAPKEE_INCLUDE_DIR}
 	)
-	target_compile_definitions(TimeCurvesTest PRIVATE NO_DLL_LINKAGE)
+	target_compile_definitions(TimeCurvesTest PRIVATE TAPKEE_EIGEN_INCLUDE_FILE=<${TAPKEE_EIGEN_INCLUDE_FILE}>)
 	add_test(NAME TimeCurvesTest COMMAND TimeCurvesTest)
 	if (MSVC)
 		string(REGEX REPLACE "/" "\\\\" QT_WIN_DLL_DIR ${QT_LIB_DIR})
-		set_tests_properties(MDSTest PROPERTIES ENVIRONMENT "PATH=${QT_WIN_DLL_DIR};$ENV{PATH}")
-		set_target_properties(MDSTest PROPERTIES VS_DEBUGGER_ENVIRONMENT "PATH=${QT_WIN_DLL_DIR};$ENV{PATH}")
+		set_tests_properties(TimeCurvesTest PROPERTIES ENVIRONMENT "PATH=${QT_WIN_DLL_DIR};$ENV{PATH};CTEST_OUTPUT_ON_FAILURE=1")
+		set_target_properties(TimeCurvesTest PROPERTIES VS_DEBUGGER_ENVIRONMENT "PATH=${QT_WIN_DLL_DIR};$ENV{PATH}")
 	else()
-		target_compile_options(MDSTest PRIVATE -fPIC)
+		target_compile_options(TimeCurvesTest PRIVATE -fPIC)
 	endif()
 	if (openiA_USE_IDE_FOLDERS)
 		set_property(TARGET TimeCurvesTest PROPERTY FOLDER "Tests")
--- modules/TimeCurves/iATimeCurvesTest.cpp
+++ modules/TimeCurves/iATimeCurvesTest.cpp
@@ -1,12 +1,40 @@
 
 #include "iASimpleTester.h"
 
-//#include "iATimeCurves.h"
+#include "tapkee/tapkee.hpp"
 
-//test if tests are run
+//#include "iATimeCurves.h"	//fails because of iAMainwindow.h include
+
+
+struct MyDistanceCallback
+{
+	tapkee::ScalarType distance(tapkee::IndexType l, tapkee::IndexType r)
+	{
+		return abs(l - r);
+	}
+};
+
+//test takpee functionality
 BEGIN_TEST
 {
-	bool testExecuted = true;
+	bool testExecuted = false;
+	tapkee::DenseMatrix* embedding = new tapkee::DenseMatrix;
+	tapkee::TapkeeOutput output;
+	Eigen::IOFormat CleanFmt(4, Eigen::DontAlignCols, ",", "\n", "", "");
+	std::stringstream buffer;
+
+	// minimal example taken from: https://tapkee.lisitsyn.me/#minimal
+	const int N = 100;
+	std::vector<tapkee::IndexType> indices(N);
+	for (int i = 0; i < N; i++) indices[i] = i;
+	MyDistanceCallback distance;
+	output = tapkee::initialize()
+							  .withParameters((tapkee::method = tapkee::MultidimensionalScaling, tapkee::target_dimension = 1))
+							  .withDistance(distance)
+							  .embedUsing(indices);
+	std::cout << "output: " << output.embedding.transpose().format(CleanFmt) << std::endl;
+
+	std::cout << "Test executed: " << testExecuted << std::endl;
 	TestAssert(testExecuted);
 }
 END_TEST
