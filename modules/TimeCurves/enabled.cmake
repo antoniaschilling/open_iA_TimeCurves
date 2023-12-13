e8513eb699bdfd1550df65fc0db3d35ea2c4df6a
antoniaschilling <schilling@fim.uni-passau.de>
2023-12-13 11:29:07 +0100

    Test setup

--- modules/TimeCurves/enabled.cmake
+++ modules/TimeCurves/enabled.cmake
@@ -1,1 +1,25 @@
 TARGET_COMPILE_DEFINITIONS(TimeCurves PRIVATE TAPKEE_EIGEN_INCLUDE_FILE=<${TAPKEE_EIGEN_INCLUDE_FILE}>)
+
+if (openiA_TESTING_ENABLED)
+	#get_filename_component(CoreSrcDir "../libs/base" REALPATH BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+
+	add_executable(TimeCurvesTest TimeCurves/iATimeCurvesTest.cpp)
+	target_link_libraries(TimeCurvesTest PRIVATE Qt${QT_VERSION_MAJOR}::Core)
+	target_include_directories(TimeCurvesTest PRIVATE 
+		${CMAKE_CURRENT_SOURCE_DIR}/../libs/base 
+		${CMAKE_CURRENT_BINARY_DIR}/../libs
+		${CMAKE_CURRENT_BINARY_DIR}           # for _export.h
+	)
+	target_compile_definitions(TimeCurvesTest PRIVATE NO_DLL_LINKAGE)
+	add_test(NAME TimeCurvesTest COMMAND TimeCurvesTest)
+	if (MSVC)
+		string(REGEX REPLACE "/" "\\\\" QT_WIN_DLL_DIR ${QT_LIB_DIR})
+		set_tests_properties(MDSTest PROPERTIES ENVIRONMENT "PATH=${QT_WIN_DLL_DIR};$ENV{PATH}")
+		set_target_properties(MDSTest PROPERTIES VS_DEBUGGER_ENVIRONMENT "PATH=${QT_WIN_DLL_DIR};$ENV{PATH}")
+	else()
+		target_compile_options(MDSTest PRIVATE -fPIC)
+	endif()
+	if (openiA_USE_IDE_FOLDERS)
+		set_property(TARGET TimeCurvesTest PROPERTY FOLDER "Tests")
+	endif()
+endif()
---
+++ modules/TimeCurves/iATimeCurvesTest.cpp
@@ -0,0 +1,12 @@
+
+#include "iASimpleTester.h"
+
+//#include "iATimeCurves.h"
+
+//test if tests are run
+BEGIN_TEST
+{
+	bool testExecuted = true;
+	TestAssert(testExecuted);
+}
+END_TEST
